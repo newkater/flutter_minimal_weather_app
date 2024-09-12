85f45ace4ca19647b6b8b477b1f6e30d7c2cfaf9
Yekaterina Korobeinikova <kate.korobeinikova@gmail.com>
2024-09-12 19:48:06 +0500

    weather animations

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_minimal_weather_app/models/city_model.dart';
 import 'package:flutter_minimal_weather_app/models/weather_model.dart';
 import 'package:flutter_minimal_weather_app/services/weather_service.dart';
+import 'package:lottie/lottie.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -37,7 +38,43 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
-  // weather animation
+  // weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) {
+      return 'assets/clear.json';
+    }
+
+    switch (mainCondition.toLowerCase()) {
+      case 'thunderstorm':
+        return 'assets/clear.json';
+      
+      case 'rain':
+        return 'assets/rain.json';
+      
+      case 'snow':
+        return 'assets/snow.json';
+      
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+      case 'sand':
+      case 'ash':
+      case 'squalls':
+      case 'tornado':
+        return 'assets/atmosphere.json';
+      
+      case 'clear':
+        return 'assets/clear.json';
+      
+      case 'clouds':
+        return 'assets/clouds.json';
+
+      default:
+        return 'assets/clear.json';
+    }
+  }
 
   // init state
   @override
@@ -56,10 +93,22 @@ class _WeatherPageState extends State<WeatherPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // city name
-            Text(_weather?.cityName ?? 'loading city..'),
+            Text('${_weather?.countryName ?? 'loading country'}: ${_weather?.cityName ?? 'loading city'}'),
 
-            //temperature
+            // animation
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+            // description
             Text('${_weather != null ? _weather?.temperature.round() : "loading weather.."}°C'),
+
+            // description
+            Text('${_weather != null ? _weather?.description : "loading weather.."}'),
+
+            // feels like
+            Text('Feels like: ${_weather != null ? _weather?.feelsLike.round() : "loading weather.."}°C'),
+
+            // wind speed
+            Text('Wind speed: ${_weather != null ? _weather?.windSpeed.round() : "loading wind speed.."} m/s'),
           ],
         ),
       ),
